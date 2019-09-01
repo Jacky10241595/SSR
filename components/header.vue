e101087418a3bec9435e4a7ee26d17f1040e8076
Jacky <505786755@qq.com>
2019-09-01 20:00:41 +0800

    1-配置以及完成公共页面头部功能

---
+++ components/header.vue
@@ -0,0 +1,69 @@
+<template>
+<!-- 头部组件 -->
+  <div class="container">
+    <!-- justify-content:space-between--左右贴边对齐 -->
+    <el-row type="flex" class="main" justify="space-between">
+      <div class="logo">
+        <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+      </div>
+      <el-row type="flex" class="navs">
+        <!-- <nuxt-link>作用与使用和router-link一样 -->
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div>
+        <nuxt-link to="/user/login">登录 / 注册</nuxt-link>
+      </div>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less">
+.container{
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 1px #ddd solid;
+    box-shadow: 0 2px 2px #ddd;
+}
+.main{
+    width: 1000px;
+    margin: 0 auto;
+}
+.navs{
+    flex: 1;
+    margin-left: 10px;
+    a{
+        display: block;
+        height: 60px;
+        padding: 0 20px;
+        box-sizing: border-box;
+
+        &:hover{
+            color: #409eff;
+            border-bottom: 5px #1b72c9 solid;
+        }
+    }
+    // 该class是nuxt会自动匹配nuxt-link的to的值,若url和to的值相等,会自动加上下面的class
+    .nuxt-link-exact-active{
+        background: #409eff;
+        color:#fff;
+
+        &:hover{
+            color: #fff;
+        }
+    }
+}
+.logo{
+    padding-top: 9px;
+    img{
+        width: 156px;
+        height:42px;
+        display: block;
+    }
+}
+</style>
---
+++ pages/tab.vue
@@ -0,0 +1,62 @@
+<template>
+    <div style="padding:50px; margin-bottom:20px;">
+        <div class="tabs">
+            <span
+            v-for="(item,index) in ['体育','新闻','娱乐']"
+            :key="index"
+            @click="handleClick(index)"
+            :class="{active:current===index}"
+            >{{item}}</span>
+        </div>
+        <div v-show="current===0">
+            <ul>
+                <li>体育</li>
+                <li>体育</li>
+                <li>体育</li>
+            </ul>
+        </div>
+        <div v-show="current===1">
+            <ul>
+                <li>新闻</li>
+                <li>新闻</li>
+                <li>新闻</li>
+            </ul>
+        </div>
+        <div v-show="current===2">
+            <ul>
+                <li>娱乐</li>
+                <li>娱乐</li>
+                <li>娱乐</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // 当前(起始)状态
+            current:0
+        }
+    },
+    methods:{
+        handleClick(index){
+            // 事件点击时修改current
+            this.current=index
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+    .tabs{
+        margin-bottom:20px;
+
+        span{
+            cursor: pointer;
+        }
+    }
+
+    .active{
+        color:hotpink;
+    }
+</style>

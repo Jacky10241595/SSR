586122232e6433ebf373571164472f7cc595ee80
Jacky <505786755@qq.com>
2019-09-07 17:06:17 +0800

    23-渲染列表,动态生成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,66 +1,82 @@
 <template>
-  <section class="contianer">
-    <el-row type="flex" justify="space-between">
-      <!-- 顶部过滤列表 -->
-      <div class="flights-content">
-        <!-- 过滤条件 -->
-        <div></div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
 
-        <!-- 航班头部布局 -->
-        <FlightsListHead />
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <div>
+                    <FlightsListHead/>
+                </div>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                     <!-- 航班列表 -->
+                    <FlightsItem
+                    v-for="(item,index) in dataList" :key="index" :data="item"/>
+                </div>
+            </div>
 
-        <!-- 航班信息 -->
-        <div>
-          <FlightsItem
-          v-for="(item,index) in flightData.flights"
-          :key="index"/>
-        </div>
-      </div>
-
-      <!-- 侧边栏 -->
-      <div class="aside">
-        <!-- 侧边栏组件 -->
-      </div>
-    </el-row>
-  </section>
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
+
 <script>
-import FlightsListHead from "@/components/air/flightsListHead.vue";
-import FlightsItem from "@/components/air/flightsItem.vue";
+
+import moment from "moment";
+import FlightsListHead from "@/components/air/flightsListHead.vue"
+import FlightsItem from "@/components/air/flightsItem.vue"
 export default {
-  data() {
-    return {
-        // 机票列表返回的总数据,总数据包含4个属性:flights/info/options/total
-        flightData:{}
-    };
-  },
-  components: {
-    FlightsListHead,
-    FlightsItem
-  },
-  mounted () {
-    //   请求航班列表数据
-    this.$axios({
-        url:'airs',
-        // 路由的url参数
-        params:this.$route.query
-    }).then(res=>{
-        // 赋值给总数据
-        this.flightData=res.data
-    })
-  }
-};
+    data(){
+        return {
+            flightsData:{}, // 航班总数据
+            dataList:[]
+        }
+    },
+    components: {
+        FlightsListHead,
+        FlightsItem
+    },
+    methods:{
+        // 获取航班总数据
+        getData(){
+            this.$axios({
+                url:`airs`,
+                params:this.$route.query
+            }).then(res=>{
+                this.flightsData=res.data;
+                this.dataList=this.flightsData.flights
+            })
+        }
+    },
+    mounted () {
+        this.getData()
+    }
+}
 </script>
-<style lang="less" scoped>
-.contianer {
-  width: 1000px;
-  margin: 20px auto;
-}
-.flights-content {
-  width: 745px;
-  font-size: 14px;
-}
-.aside {
-  width: 240px;
-}
+
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
+
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>

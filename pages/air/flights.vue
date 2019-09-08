efe05305bbaece8a6836a722e839758a871275db
Jacky <505786755@qq.com>
2019-09-08 11:31:06 +0800

    27-新建过滤组件

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,7 +4,9 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <div></div>
+        <div>
+          <FlightsFilters />
+        </div>
 
         <!-- 航班头部布局 -->
         <div>
@@ -48,6 +50,7 @@
 import moment from "moment";
 import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
+import FlightsFilters from "@/components/air/flightsFilters.vue";
 export default {
   data() {
     return {
@@ -59,6 +62,7 @@ export default {
   },
   components: {
     FlightsListHead,
+    FlightsFilters,
     FlightsItem
   },
   methods: {

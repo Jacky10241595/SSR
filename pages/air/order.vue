c41d243174995dbe6f118c523701409d8f6cde59
Jacky <505786755@qq.com>
2019-09-09 15:27:09 +0800

    33-机票订单首页布局

---
+++ pages/air/order.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 机票订单首页组件 -->
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main"></div>
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped>
+.container{
+    width: 1000px;
+    margin: 20px auto;
+}
+.aside{
+    width: 350px;
+    height: fit-content;
+    border:1px solid #ddd;
+}    
+</style>

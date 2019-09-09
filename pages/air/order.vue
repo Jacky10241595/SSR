64c18808b56442ddc45591719a461236b5a3a41a
Jacky <505786755@qq.com>
2019-09-09 16:55:14 +0800

    33-机票订单表单组件

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,30 +1,38 @@
 <template>
-  <!-- 机票订单首页组件 -->
-  <div class="container">
-    <el-row type="flex" justify="space-between">
-      <!-- 订单表单 -->
-      <div class="main"></div>
+    <div class="container">
+        <el-row type="flex" justify="space-between">
+            <!-- 订单表单 -->
+            <div class="main">
+                <OrderForm/>
+            </div>
 
-      <!-- 侧边栏 -->
-      <div class="aside"></div>
-    </el-row>
-  </div>
+            <!-- 侧边栏 -->
+            <div class="aside">
+                          
+            </div>
+        </el-row>
+    </div>
 </template>
+
 <script>
+import OrderForm from "@/components/air/orderForm.vue";
 export default {
-  data() {
-    return {};
-  }
-};
-</script>
-<style lang="less" scoped>
-.container{
-    width: 1000px;
-    margin: 20px auto;
+    components: {
+        OrderForm
+    }
 }
-.aside{
-    width: 350px;
-    height: fit-content;
-    border:1px solid #ddd;
-}    
+</script>
+
+<style lang="less" scoped>
+    .container{
+        width:1000px;
+        margin:20px auto;
+    }
+    
+    /*aside*/
+    .aside{
+        width: 350px;
+        height: fit-content;
+        border:1px #ddd solid;
+    }
 </style>
